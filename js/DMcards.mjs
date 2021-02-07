f394cdb92e182992452a880dfa5db08d51c14589
Anshuman <amedhi@connect.ust.hk>
2021-02-07 21:46:04 +0800

    helper functions for cards

--- js/DMcards.mjs
+++ js/DMcards.mjs
@@ -1,29 +1,58 @@
 import { Shield, DamagingShield } from "DMshields.js";
-// TODO:
+
 export class DMCard {
     constructor(
         name,
-        shieldValue,
-        healValue,
-        dmgValue,
-        actionsValue,
+        shieldValue = 0,
+        healValue = 0,
+        dmgValue = 0,
+        extraActions = 0,
         extraPowers = []
     ) {
         this.name = name;
         this.shieldValue = shieldValue;
         this.healValue = healValue;
         this.dmgValue = dmgValue;
-        this.actionsValue = actionsValue;
+        this.extraActions = extraActions;
         this.extraPowers = extraPowers;
         this.animation = {}; //TODO: figure out how to handle animations
     }
 
+    static shieldCard(name, amount) {
+        return DMCard(name, amount, 0, 0, 0);
+    }
+    static healCard(name, amount) {
+        return DMCard(name, 0, amount, 0, 0);
+    }
+    static damageCard(name, amount) {
+        return DMCard(name, 0, 0, amount, 0);
+    }
+    static actionCard(name, amount) {
+        return DMCard(name, 0, 0, 0, amount);
+    }
     static powerCard(name, power, extraPowers = []) {
         return DMCard(name, 0, 0, 0, 0, [power].concat(extraPowers));
     }
 
+    addHeal(amount) {
+        this.healValue = amount;
+        return this;
+    }
+    addShield(amount) {
+        this.shieldValue = amount;
+        return this;
+    }
+    addDamage(amount) {
+        this.dmgValue = amount;
+        return this;
+    }
+    addActions(amount) {
+        this.extraActions = amount;
+        return this;
+    }
+
     /**
-     * Play the card, override this to give the card unique actions
+     * Play the card
      *
      * @param {*} player The object of the player playing this card rn
      * See DMplayer.js
@@ -37,8 +66,8 @@ export class DMCard {
         if (this.healValue != 0) {
             player.character.heal(this.healValue);
         }
-        if (this.actionsValue != 0) {
-            player.addExtraAction(this.actionsValue);
+        if (this.extraActions != 0) {
+            player.addExtraAction(this.extraActions);
         }
         if (this.dmgValue != 0) {
             const targets = context.choosePlayer();
