4bb5e625f1458c6775e60b60f9ae421213e9922e
Anshuman <amedhi@connect.ust.hk>
2021-05-11 21:12:08 +0800

    production mode breaks stuff!?

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,6 +1,6 @@
 const config = require("../webpack.config.js");
 const path = require("path")
 
-config.mode = "production";
+//config.mode = "production";
 config.output.path = path.resolve(__dirname, "..");
 module.exports = config;
